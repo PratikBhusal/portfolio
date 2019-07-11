f09ad5409707d3c58f6db25ab33f16a1aa29ac26
PratikBhusal <PratikBhusal@users.noreply.github.com>
2019-07-10 21:10:02 -0500

    Cleaned up plugin and tasks

--- gulpfile.js
+++ gulpfile.js
@@ -2,67 +2,74 @@
 
 // Plugins {{{
 const gulp = require('gulp');
-const browserSync = require('browser-sync').create();
-
-const sass = require('gulp-sass');
-const ghPages = require('gh-pages');
-const sourcemaps = require('gulp-sourcemaps');
-const cleanCSS = require('gulp-clean-css');
-// const plugins = require('gulp-load-plugins')({
-//     rename: {
-//         'gulp-clean-css': 'cleanCSS'
-//     }
-// });
+const plugins = require('gulp-load-plugins')({
+    rename: {
+        'gulp-clean-css': 'cleanCSS'
+    }
+});
+const extendedPlugins = require('gulp-load-plugins')({
+    overridePattern: false,
+    pattern: ['gh-pages', 'del', 'browser-sync'],
+});
 // Plugins }}}
 
-function deploy() {
+// Clean built files
+function clean() {
+    return extendedPlugins.del(["dist/style"]);
+}
+
+function publish_site() {
     // Deploy dist folder to master branch
-    return ghPages.publish('dist', {
+    return extendedPlugins.ghPages.publish('dist', {
         message: "See branch 'gh-pages' for detailed changes",
         branch: 'master'
     });
 }
 
 // Compile SCSS into CSS
-function style() {
+function css() {
+    // Remove any unncessary files
+    clean();
+
     // Find .scss File(s)
     return gulp.src('src/scss/main.scss')
 
     // Init Source maps
-    .pipe(sourcemaps.init())
+    .pipe(plugins.sourcemaps.init())
 
     // Pass .scss file(s) to sass compiler. Return any errors if they exist
-    .pipe( sass({outputStyle: 'compressed'}).on('error', sass.logError) )
+    .pipe( plugins.sass({outputStyle: 'compressed'}).on(
+        'error', plugins.sass.logError)
+    )
 
     // Clean up the CSS
-    .pipe(cleanCSS({level: 2}))
+    .pipe( plugins.cleanCSS({level: 2}) )
 
     // Create source maps
-    .pipe( sourcemaps.write('.') )
+    .pipe( plugins.sourcemaps.write('.') )
 
 
     // Save compiled .css files to dist/style
     .pipe( gulp.dest('dist/style') )
 
     // Steam to web browser(s)
-    .pipe(browserSync.stream());
+    .pipe( extendedPlugins.browserSync.stream() );
 }
 
-function watch(done) {
-    browserSync.init({
+function watch() {
+    extendedPlugins.browserSync.init({
         server: {
             baseDir: 'dist/'
         },
+        // port: 6411,
         notify: false
     });
-    gulp.watch('src/scss/**/*.scss', style);
-    gulp.watch('src/js/**/*.js').on('change', browserSync.reload);
-    gulp.watch('dist/**/*.html').on('change', browserSync.reload);
-    gulp.watch('dist/js/**/*.js').on('change', browserSync.reload);
-    done();
+    gulp.watch('src/scss/**/*.scss', css);
+    gulp.watch('dist/**/*.html').on('change', extendedPlugins.browserSync.reload);
 }
 
-exports.deploy = deploy;
-exports.style = style;
+exports.clean = clean;
+exports.build = gulp.series(clean, gulp.parallel(css));
+exports.deploy = gulp.series(exports.build, publish_site);
 exports.watch = watch;
 exports.default = watch;
